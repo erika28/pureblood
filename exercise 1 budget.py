0f798b14c71492deba8e2cd0bfa1b721bfe87b35
erika28 <36615900+erika28@users.noreply.github.com>
2018-02-19 16:12:33 +0800

    Add files via upload

---
+++ exercise 1 budget.py
@@ -0,0 +1,13 @@
+import sys
+
+print "Please state your budget for a proposal on the new high end speakers"
+z= input (":")
+print "One high end speakers would cost", z,": "
+
+budget = input(":")
+x= budget/z
+print "You can afford", x, "unit/s of speakers"
+y= budget-(x*z)
+print "Buying this number of unit/s would leave you", y, "pesos from your savings"
+
+                                              
---
+++ exercise 2 factorial.py
@@ -0,0 +1,7 @@
+print  "Input a number from :"
+x=input("what is the number: ")
+sum=1
+for x in range (1,x+1,+1):
+    sum =sum*x
+print ("factorial of your given number is:", sum)
+    
